9dc180db767c791d31d7bcff962f6ae59de04643
esikkala <esko.ikkala@aalto.fi>
2019-04-09 10:50:33 +0300

    Clean up

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import SemanticPortal from '../containers/SemanticPortal';
+import deepPurple from '@material-ui/core/colors/deepPurple';
 // import green from '@material-ui/core/colors/green';
 // import deepOrange from '@material-ui/core/colors/deepOrange';
-import deepPurple from '@material-ui/core/colors/deepPurple';
 // import red from '@material-ui/core/colors/red';
 // import amber from '@material-ui/core/colors/amber';
 // import grey from '@material-ui/core/colors/grey';
