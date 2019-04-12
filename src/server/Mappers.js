517852860954002049b237a0da6faa780aff817b
esikkala <esko.ikkala@aalto.fi>
2019-04-12 11:15:22 +0300

    Remove result mapper tweaks, use id as the main SPARQL variable

--- src/server/Mappers.js
+++ src/server/Mappers.js
@@ -1,15 +1,11 @@
 import {
-  flatten,
   forOwn,
-  isEqual,
   get,
   reduce,
-  find,
-  compact
 }
   from 'lodash';
 
-export const groupBy = (sparqlBindings, group, simplify) => Object.values(reduce(sparqlBindings, (results, sparqlResult) => {
+export const groupBy = (sparqlBindings, group) => Object.values(reduce(sparqlBindings, (results, sparqlResult) => {
   const id = get(sparqlResult[group], 'value');
   if (id === undefined) {
     return [];
@@ -22,42 +18,15 @@ export const groupBy = (sparqlBindings, group, simplify) => Object.values(reduce
     if (key === group) {
       result[group] = value.value;
     } else {
-      if (simplify) {
-        if (key === 'basicElement' || key === 'markerColor') {
-          result[key] = value.value;
-        } else {
-          result[key] = capitalizeFirstLetter(value.value);
-        }
-      } else {
-        const oldVal = result[key];
-        // add new value if it doesn't already exist
-        if (!find(oldVal, (val) => isEqual(val, value))) {
-          (result[key] || (result[key] = [])).push(value);
-        }
-      }
+      result[key] = value.value;
+      //const oldVal = result[key];
+      // add new value if it doesn't already exist
+      //if (!find(oldVal, (val) => isEqual(val, value))) {
+      //  (result[key] || (result[key] = [])).push(value);
+      //}
     }
   });
   return results;
 }, {}));
 
-export const mergeSuggestions = (suggestions) => {
-  return groupBy(compact(flatten(suggestions)), 'label', false);
-};
-
-
-export const mergeSimpleSuggestions = (suggestions) => {
-
-  // Suggestions from different datasets may have duplicates
-  let uniqueSuggestions = [...new Set(flatten(suggestions))];
-
-  // Sort suggestions alphabetically, because Lunece score does
-  // not work with wildcard queries.
-  return uniqueSuggestions.sort();
-};
-
-
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-export const mapResults = (results) => groupBy(results, 's', true);
+export const mapResults = (results) => groupBy(results, 'id');
