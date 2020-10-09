ec90337c960de9401047114e2ea09c7878c748c8
esikkala <esko.ikkala@aalto.fi>
2020-10-09 17:18:12 +0300

    Clean up

--- src/client/components/facet_results/LeafletMap.js
+++ src/client/components/facet_results/LeafletMap.js
@@ -6,7 +6,7 @@ import L from 'leaflet'
 import { has, orderBy, isEqual } from 'lodash'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { purple } from '@material-ui/core/colors'
-import { MAPBOX_ACCESS_TOKEN, MAPBOX_STYLE } from '../../configs/findsampo/GeneralConfig'
+// import { MAPBOX_ACCESS_TOKEN, MAPBOX_STYLE } from '../../configs/namesampo/GeneralConfig'
 // import { apiUrl } from '../../epics'
 import 'leaflet/dist/leaflet.css' // Official Leaflet styles
 import './LeafletMap.css' // Customizations to Leaflet styles
